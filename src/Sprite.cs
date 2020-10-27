231e7c3f01e831875d971528bdb5f4273283e960
CMH <CMH@MLHs-MacBook-Pro.local>
2020-10-27 17:58:33 -0400

    Made Sprite.cs and used inheritance.

---
+++ src/Sprite.cs
@@ -0,0 +1,74 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ArcadeFlyer2D
+{
+
+    class Sprite
+    {
+        protected Vector2 position;
+        public Vector2 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+            }
+        }
+        private Texture2D spriteImage;
+        public Texture2D SpriteImage;
+        {
+            get
+            {
+                return SpriteImage;
+            }
+            set 
+            {
+                SpriteImage = value;
+            }
+        }
+        private float spriteWidth;
+        public float SpriteWidth;
+        {
+            get
+            {
+                return SpriteWidth;
+            }
+            set 
+            {
+                SpriteWidth = value;
+            }
+        }
+
+        public float SpriteHeight
+        {
+            get
+            {
+                float scale = spriteWidth / spriteImage.Width;
+                return spriteImage.Height * scale;
+            }
+        }
+        
+        public Rectangle PositionRectangle
+        {
+            get
+            {
+                return new Rectangle((int)position.X, (int)position.Y, (int)spriteWidth, (int)SpriteHeight);
+            }
+        }
+
+        public Sprite(Vector2 position)
+        {
+            this.position = position;
+        }
+
+         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spriteImage, PositionRectangle, Color.White);
+        }
+
+    }
+}
