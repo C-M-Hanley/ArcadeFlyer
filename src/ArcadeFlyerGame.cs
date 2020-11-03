5b3cbb4116b4fa22c1f2c4ff443abb2b97544929
CMH <CMH@MLHs-MacBook-Pro.local>
2020-11-03 15:51:31 -0500

    Added Enemy.cs and Sprite.cs

--- src/ArcadeFlyerGame.cs
+++ src/ArcadeFlyerGame.cs
@@ -17,6 +17,8 @@ namespace ArcadeFlyer2D
 
         private Player player;
 
+        private Enemy enemy;
+
         private int screenWidth = 1600;
         public int ScreenWidth
         {
@@ -51,6 +53,7 @@ namespace ArcadeFlyer2D
 
             Vector2 posistion = new Vector2(0.0f, 0.0f);
             player = new Player(this, posistion);
+            enemy = new Enemy(this, new Vector2(screenWidth, 0));
         }
 
         // Initialize
@@ -71,6 +74,7 @@ namespace ArcadeFlyer2D
         protected override void Update(GameTime gameTime)
         {   
             player.Update(gameTime);
+            enemy.Update(gameTime);
 
             // Update base game
             base.Update(gameTime);
@@ -89,6 +93,7 @@ namespace ArcadeFlyer2D
             // spriteBatch.Draw(playerImage, playerDestinationRect, Color.White);
 
             player.Draw(gameTime, spriteBatch);
+            enemy.Draw(gameTime, spriteBatch);
 
             spriteBatch.End();
         }
--- src/Enemy.cs
+++ src/Enemy.cs
@@ -4,48 +4,25 @@ using Microsoft.Xna.Framework.Input;
 
 namespace ArcadeFlyer2D
 {
-    class Enemy
+    class Enemy : Sprite
     {
         private ArcadeFlyerGame root;
 
-        private Vector2 position;
-
-        private Texture2D spriteImage;
-
-        private float spriteWidth;
 
         private Vector2 velocity;
 
-        public float SpriteHeight
-        {
-            get
-            {
-                float scale = spriteWidth / spriteImage.Width;
-                return spriteImage.Height * scale;
-            }
-        }
-
-         public Rectangle PositionRectangle
-        {
-            get
-            {
-                return new Rectangle((int)position.X, (int)position.Y, (int)spriteWidth, (int)SpriteHeight);
-            }
-        }
-
-
-        public Enemy(ArcadeFlyerGame root, Vector2 position)
+        public Enemy(ArcadeFlyerGame root, Vector2 position) : base(position)
         {
             this.root = root;
             this.position = position;
-            this.spriteWidth = 128.0f;
+            this.SpriteWidth = 128.0f;
             this.velocity = new Vector2(-1.0f, 5.0f);
 
             LoadContent();
         }
 
             public void LoadContent(){
-                this.spriteImage = root.Content.Load<Texture2D>("Enemy");
+                this.SpriteImage = root.Content.Load<Texture2D>("rhino");
             }
 
             public void Update(GameTime gameTime)
@@ -55,11 +32,6 @@ namespace ArcadeFlyer2D
             {
                 velocity.Y *= -1;
             }
-        }
-
-             public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(spriteImage, PositionRectangle, Color.White);
         }
     }
 }
